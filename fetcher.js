08d3a846a21249cabfeb573ad9b349ccfa9a36f7
Alan <alanmak95@gmail.com>
2020-12-03 03:16:18 +0000

    Eslint check

--- fetcher.js
+++ fetcher.js
@@ -13,27 +13,27 @@ request(url, (error, response, body) => {
     console.log("Bad URL");
     process.exit();
   } else if (path === undefined) {
-    console.log("Invalid local path")
+    console.log("Invalid local path");
     process.exit();
   } else {
-    if(!fs.existsSync(path)) {
+    if (!fs.existsSync(path)) {
       fs.writeFile(path, body, error => {
         if (error) throw error;
-        console.log(`Download and saved ${body.length} byte to ${path}`)
-      })
+        console.log(`Download and saved ${body.length} byte to ${path}`);
+      });
     } else if (fs.existsSync(path)) {
       rl.question('Do you want to override the file? Press "y" if you do ... ', key => {
         if (key === 'y') {
           fs.writeFile(path, body, error => {
             if (error) throw error;
-            console.log(`Download and saved ${body.length} byte to ${path}`)
+            console.log(`Download and saved ${body.length} byte to ${path}`);
             rl.close();
-          })
+          });
         } else {
-          console.log('Y not pressed...Not saved... goodbye')
+          console.log('Y not pressed...Not saved... goodbye');
           rl.close();
         }
-      })
+      });
     }
   }
 });
