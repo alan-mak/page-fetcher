51e299b3bf00fbe333f2b0c1519517d21095ad2e
Alan <alanmak95@gmail.com>
2020-12-03 03:15:06 +0000

    Added 3 edge cases

--- fetcher.js
+++ fetcher.js
@@ -1,22 +1,39 @@
 let [url, path] = process.argv.slice(2);
-let directory = './';
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 let fs = require('fs');
 const request = require('request');
 
 request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (!fs.existsSync(path)) {
-    // fs.createWriteStream(directory + path, {flags: 'w',encoding: 'utf8'})
-    fs.writeFile(directory + path, body, error => {
-      if (error) throw error;
-      console.log(`Download and saved ${body.length} byte to ${path}`)
-    })
-    // console.log(Buffer.byteLength(body))
+  if (error || response === undefined || response.statusCode !== 200) {
+    console.log("Bad URL");
+    process.exit();
+  } else if (path === undefined) {
+    console.log("Invalid local path")
+    process.exit();
   } else {
-
+    if(!fs.existsSync(path)) {
+      fs.writeFile(path, body, error => {
+        if (error) throw error;
+        console.log(`Download and saved ${body.length} byte to ${path}`)
+      })
+    } else if (fs.existsSync(path)) {
+      rl.question('Do you want to override the file? Press "y" if you do ... ', key => {
+        if (key === 'y') {
+          fs.writeFile(path, body, error => {
+            if (error) throw error;
+            console.log(`Download and saved ${body.length} byte to ${path}`)
+            rl.close();
+          })
+        } else {
+          console.log('Y not pressed...Not saved... goodbye')
+          rl.close();
+        }
+      })
+    }
   }
 });
